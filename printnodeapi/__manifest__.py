98ce0850dec8810821f785a8d4e017385f505c31
xLeuname <em.mendoza.datalexora@gmail.com>
2025-03-28 02:52:21 +0800

    Update __manifest__.py

--- printnodeapi/__manifest__.py
+++ printnodeapi/__manifest__.py
@@ -6,6 +6,9 @@
     'category': 'Tools',
     'license': 'LGPL-3',
     'depends': ['base'],
+    'external_dependencies': {
+        'python': ['future'],
+    },
     'data': [],
     'installable': True,
     'application': False,
