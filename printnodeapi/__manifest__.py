89233f024e94ebd14e0561cb342b1e53f74cdcec
xLeuname <em.mendoza.datalexora@gmail.com>
2025-03-28 02:35:27 +0800

    Add files via upload

---
+++ printnodeapi/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'PrintNode Odoo Integration',
+    'version': '1.0',
+    'summary': 'Integrates PrintNode API with Odoo',
+    'author': 'Your Name',
+    'category': 'Tools',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
---
+++ printnodeapi/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(
+    name='PrintNodeApi',
+    version='0.2.0',
+    description="Python Client for PrintNode's API",
+    author='printnode',
+    author_email='support@printnode.com',
+    packages=['printnodeapi'],
+    install_requires=['requests','future'])
